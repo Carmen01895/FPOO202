a326ff40593c9736f6e90c73b0048d42f1663622
María Carmen Atilano García <123046171@upq.edu.mx>
2025-01-28 14:47:46 -0600

    Ejercicio 1

---
+++ Sintaxis/src/Ejercicio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner cns = new Scanner(System.in);
+        System.out.println("Ingrese las horas que trabajo esta semana: ");
+        int horas = Integer.parseInt(cns.nextLine());
+        double pagohoras = (double)horas * (double)39.5F;
+        System.out.println("La pago es $" + pagohoras);
+    }
+}
