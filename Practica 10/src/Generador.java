f590db12be101c9d0acf698d9ae4831b952f1512
María Carmen Atilano García <123046171@upq.edu.mx>
2025-02-27 15:35:34 -0600

    Cambios en practica 10
    En la clase Generador, cambiamos nuestros atributos a privados para luego crear nuestros getters & setters y hacer nuestro constructor.

--- Practica 10/src/Generador.java
+++ Practica 10/src/Generador.java
@@ -2,17 +2,122 @@ import java.security.SecureRandom;
 import javax.swing.JOptionPane;
 
 public class Generador {
-    public String minusculas = "abcdefghijklmnñopqrstuvwxyz";
-    String mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-    String numeros = "0123456789";
-    String especiales = "!@#$%&*";
-    int longitud = 8; // Longitud por defecto
-    boolean incluirMayusculas = false;
-    boolean incluirEspeciales = false;
+    private String minusculas = "abcdefghijklmnñopqrstuvwxyz";
+    private String mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    private String numeros = "0123456789";
+    private String especiales = "!@#$%&*";
+    private int longitud = 8;
+    private boolean incluirMayusculas = false;
+    private boolean incluirEspeciales = false;
+    private String input;
+    private int respuestaMayusculas;
+    private int respuestaEspeciales;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public int getRespuestaMayusculas() {
+        return respuestaMayusculas;
+    }
+
+    public void setRespuestaMayusculas(int respuestaMayusculas) {
+        this.respuestaMayusculas = respuestaMayusculas;
+    }
+
+    public int getRespuestaEspeciales() {
+        return respuestaEspeciales;
+    }
+
+    public void setRespuestaEspeciales(int respuestaEspeciales) {
+        this.respuestaEspeciales = respuestaEspeciales;
+    }
+
+
+
+
+    public String getMinusculas() {
+        return minusculas;
+    }
+
+    public void setMinusculas(String minusculas) {
+        this.minusculas = minusculas;
+    }
+
+    public String getMayusculas() {
+        return mayusculas;
+    }
+
+    public void setMayusculas(String mayusculas) {
+        this.mayusculas = mayusculas;
+    }
+
+    public String getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(String numeros) {
+        this.numeros = numeros;
+    }
+
+    public String getEspeciales() {
+        return especiales;
+    }
+
+    public void setEspeciales(String especiales) {
+        this.especiales = especiales;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public boolean isIncluirMayusculas() {
+        return incluirMayusculas;
+    }
+
+    public void setIncluirMayusculas(boolean incluirMayusculas) {
+        this.incluirMayusculas = incluirMayusculas;
+    }
+
+    public SecureRandom getRandom() {
+        return random;
+    }
+
+    public void setRandom(SecureRandom random) {
+        this.random = random;
+    }
+
+    public boolean isIncluirEspeciales() {
+        return incluirEspeciales;
+    }
+
+    public void setIncluirEspeciales(boolean incluirEspeciales) {
+        this.incluirEspeciales = incluirEspeciales;
+    }
+
+    public Generador() {
+        this.minusculas = minusculas;
+        this.mayusculas = mayusculas;
+        this.numeros = numeros;
+        this.especiales = especiales;
+        this.longitud = longitud;
+        this.incluirMayusculas = incluirMayusculas;
+        this.incluirEspeciales = incluirEspeciales;
+    }
+
     SecureRandom random = new SecureRandom();
 
     public String generar() {
-        String caracteres = minusculas + numeros; // Caracteres base
+        String caracteres = minusculas + numeros;
 
         if (incluirMayusculas) {
             caracteres += mayusculas;
