2de0097fe98f7d447e7a20c09f4df21abd470cd6
María Carmen Atilano García <123046171@upq.edu.mx>
2025-02-27 14:53:30 -0600

    Cambios en practica 9 atributos, getters & setters y constructor
    En la clase Administración, cambiamos nuestros atributos a privados para luego crear nuestros getters & setters y hacer nuestro constructor.

--- Practica9/src/Administracion.java
+++ Practica9/src/Administracion.java
@@ -2,16 +2,54 @@ import javax.swing.*;
 
 public class Administracion {
 
-    int cuenta;
-    String titular;
-    int edad;
-    String saldo;
+    private int cuenta;
+    private String titular;
+    private int edad;
+    private String saldo;
 
-    public Object consulta(){
+    public int getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(int cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+
+    public Administracion() {
+        this.cuenta = cuenta;
+        this.titular = titular;
+        this.edad = edad;
+        this.saldo = saldo;
+    }
+
+    public void consulta(){
         double saldoFinal = Double.parseDouble(saldo);
         JOptionPane.showMessageDialog(null,  "Cuenta: " + cuenta + "\n"
-        + "Titular: " + titular + "\n" + "Edad: " + edad + "\n" + "Saldo: $" + saldoFinal,"Consulta",1);
-        return null;
+        + "Titular: " + titular + "\n" + "Edad: " + edad + "\n" + "Saldo: $" + saldoFinal,"Consulta", JOptionPane.INFORMATION_MESSAGE);
     }//llaves consulta
     public void ingresa() {
         String monto = JOptionPane.showInputDialog(null,
@@ -21,7 +59,7 @@ public class Administracion {
         double saldoFinal = Double.parseDouble(saldo);
         double total = saldoFinal + montoFinal;
         JOptionPane.showMessageDialog(null,  "Saldo: $" + saldoFinal + "\n"
-                + "Monto: $" + montoFinal + "\n" + "Nuevo saldo: $" + total,"Deposito a su Cuenta",1);
+                + "Monto: $" + montoFinal + "\n" + "Nuevo saldo: $" + total,"Deposito a su Cuenta", JOptionPane.INFORMATION_MESSAGE);
     }//llaves ingresar
     public void retirar() {
         String monto = JOptionPane.showInputDialog(null,
@@ -31,7 +69,7 @@ public class Administracion {
         double saldoFinal = Double.parseDouble(saldo);
         double total = saldoFinal - montoFinal;
         JOptionPane.showMessageDialog(null,  "Saldo: $" + saldoFinal + "\n"
-                + "Monto retirado: $" + montoFinal + "\n" + "Saldo restante: $" + total,"Retiro de su Cuenta",1);
+                + "Monto retirado: $" + montoFinal + "\n" + "Saldo restante: $" + total,"Retiro de su Cuenta", JOptionPane.INFORMATION_MESSAGE);
     }//llaves retirar
     public void depositoOtro() {
         String cuentaDes = JOptionPane.showInputDialog(null,
@@ -47,7 +85,7 @@ public class Administracion {
         double montoFinal = Double.parseDouble(monto);
         double total = saldoFinal - montoFinal;
         JOptionPane.showMessageDialog(null,  "Saldo: $" + saldoFinal + "\n"
-                + "Monto depositado: $" + montoFinal + "\n" + "Saldo Restante: $" + total,"Deposito a la Cuenta" + cuentaDes,1);
+                + "Monto depositado: $" + montoFinal + "\n" + "Saldo Restante: $" + total,"Deposito a la Cuenta" + cuentaDes, JOptionPane.INFORMATION_MESSAGE);
 
     }
 }//llaves de clase
