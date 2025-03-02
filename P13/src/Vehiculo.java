9614fe8a6014c9ec87512ef75ce8a72d4f1d61b7
María Carmen Atilano García <123046171@upq.edu.mx>
2025-03-01 23:20:15 -0600

    Actualización de la clase Vehículo
    Actualizamos la clase Vehículo para colocar un atributo privado para asignar el conductor

--- P13/src/Vehiculo.java
+++ P13/src/Vehiculo.java
@@ -5,6 +5,15 @@ public class Vehiculo {
     private String placa;
     private String modelo;
     private double capacidad;
+    private Conductor conductor;
+
+    public Conductor getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Conductor conductor) {
+        this.conductor = conductor;
+    }
 
     public String getPlaca() {
         return placa;
@@ -34,11 +43,16 @@ public class Vehiculo {
         this.placa = placa;
         this.modelo = modelo;
         this.capacidad = capacidad;
+        this.conductor = conductor;
     }
 
     public void RegistrarVehiculo() {
-        placa = JOptionPane.showInputDialog(null, "Ingrese la Placa del Vehiculo: ", "Vehiculo", JOptionPane.INFORMATION_MESSAGE);
-        modelo = JOptionPane.showInputDialog(null, "Ingrese el modelo del Vehiculo: ", "Vehiculo", JOptionPane.INFORMATION_MESSAGE);
-        capacidad = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la capacidad de carga del Vihiculo: ", "Vehiculo", JOptionPane.INFORMATION_MESSAGE));
+        placa = JOptionPane.showInputDialog(null, "Ingrese la Placa del Vehiculo: ", "Vehiculo", JOptionPane.PLAIN_MESSAGE);
+        modelo = JOptionPane.showInputDialog(null, "Ingrese el modelo del Vehiculo: ", "Vehiculo", JOptionPane.PLAIN_MESSAGE);
+        capacidad = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la capacidad de carga del Vihiculo: ", "Vehiculo", JOptionPane.PLAIN_MESSAGE));
+    }
+
+    public void AsignarConductor() {
+         JOptionPane.showInputDialog(null, "Ingrese el nombre de su Conductor: " , "Asignar Conductor", JOptionPane.PLAIN_MESSAGE);
     }
 }
